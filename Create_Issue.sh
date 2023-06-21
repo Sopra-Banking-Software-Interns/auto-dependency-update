ac0e0b2d478483ddd1034473d2ebcdbcd73beddb
navvay <135510092+navvay@users.noreply.github.com>
2023-06-21 15:59:23 +0530

    Update Create_Issue.sh

--- Create_Issue.sh
+++ Create_Issue.sh
@@ -5,7 +5,7 @@ REPO_URL="https://github.com/Sopra-Banking-Software-Interns/auto-dependency-upda
 
 # GitHub personal access token
 GITHUB_TOKEN=$token
-# echo $GITHUB_TOKEN
+
 # Issue title and body
 ISSUE_TITLE="Available version updates"
 
@@ -17,35 +17,54 @@ for line in "${lines[@]}"; do
   ISSUE_BODY+="\n$line"
 done
 
-#ISSUE_BODY=$(cat testing.txt)
-#echo $ISSUE_BODY
 # Extract the owner and repository name from the URL
 REPO_OWNER=$(echo "$REPO_URL" | awk -F/ '{print $(NF-1)}')
 REPO_NAME=$(echo "$REPO_URL" | awk -F/ '{print $NF}' | sed 's/.git$//')
-# Create the issue using the GitHub API
-response=$(curl -H "Authorization: Bearer $GITHUB_TOKEN" \
+
+# Get the list of repository owners
+owners_response=$(curl -H "Authorization: Bearer $GITHUB_TOKEN" \
   -H "Accept: application/vnd.github.v3+json" \
-  -d "{\"title\":\"$ISSUE_TITLE\",\"body\":\"$ISSUE_BODY\"}" \
+  "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/collaborators?affiliation=owner")
+
+# Check if the response is valid JSON
+if ! jq -e . >/dev/null 2>&1 <<<"$owners_response"; then
+  echo "Failed to get repository owners"
+  echo "API response:"
+  echo "$owners_response"
+  exit 1
+fi
+
+# Extract the usernames of the owners
+usernames=$(echo "$owners_response" | jq -r '.[].login')
+
+# Select a random owner
+random_owner=$(shuf -e $usernames | head -n 1)
+
+# Create the issue using the GitHub API
+issue_response=$(curl -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -H "Accept: application/vnd.github.v3+json" \
+  -d "{\"title\":\"$ISSUE_TITLE\",\"body\":\"$ISSUE_BODY\",\"assignees\":[\"$random_owner\"]}" \
   "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/issues")
 
 # Check if the response is valid JSON
-if ! jq -e . >/dev/null 2>&1 <<<"$response"; then
+if ! jq -e . >/dev/null 2>&1 <<<"$issue_response"; then
   echo "Failed to create the issue"
   echo "API response:"
-  echo "$response"
+  echo "$issue_response"
   exit 1
 fi
 
 # Extract the issue number from the response
-issue_number=$(echo "$response" | jq -r '.number')
+issue_number=$(echo "$issue_response" | jq -r '.number')
 
 # Check if the issue number is null
 if [[ "$issue_number" == "null" ]]; then
   echo "Failed to create the issue"
   echo "API response:"
-  echo "$response"
+  echo "$issue_response"
   exit 1
 fi
 
-# Display the created issue number
+# Display the created issue number and the assigned owner
 echo "New issue created: #$issue_number"
+echo "Assigned to: $random_owner"
