7b0ab69cd95dede5ceb7a8ff88944b6e536a15e3
Priyansh <100060048+Riyu44@users.noreply.github.com>
2023-06-22 14:50:25 +0530

    Update upd_req2updates.sh

--- upd_req2updates.sh
+++ upd_req2updates.sh
@@ -4,10 +4,10 @@ check=0
 while IFS= read -r line || [ -n "$line" ]; do
 line=$(echo "$line" | awk '{$1=$1};1')
   if [[ "$line" == "~UPD" && $check -eq 0 ]]; then
-    # echo "Flag ~UPD found in the first line. Performing actions..."
+    echo "Flag ~UPD found in the first line. Performing actions..."
     check=1
   elif [[ $check -eq 0 ]]; then
-    # echo "Flag ~UPD not found. Aborting..."
+    echo "Flag ~UPD not found. Aborting..."
     break
   else
     echo "$line" >> updates.txt
