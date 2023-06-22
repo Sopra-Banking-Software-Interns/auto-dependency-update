540cfd11816fbac624e24db637315ca8d5f0bd08
navvay <135510092+navvay@users.noreply.github.com>
2023-06-22 15:16:31 +0530

    Update read_comment.sh

--- read_comment.sh
+++ read_comment.sh
@@ -3,6 +3,7 @@
 TOKEN=$token
 
 echo "Here's the new comment that I found:"
+
 # Extract the username and repository name from the GitHub URL
 URL="https://github.com/Sopra-Banking-Software-Interns/auto-dependency-update"
 USERNAME=$(echo "$URL" | sed -n 's#https://github.com/\([^/]*\)/\([^/]*\).*#\1#p')
@@ -12,7 +13,7 @@ REPO=$(echo "$URL" | sed -n 's#https://github.com/\([^/]*\)/\([^/]*\).*#\2#p')
 API="https://api.github.com"
 
 # Fetch all comments
-comments=$(curl -sSL -H "Authorization: token $TOKEN" "$API/repos/$USERNAME/$REPO/issues/comments")
+comments=$(curl -sSL -H "Authorization: token $TOKEN" "$API/repos/$USERNAME/$REPO/issues/comments?per_page=100")
 
 # Check if comments is empty
 if [[ -z "$comments" ]]; then
@@ -20,14 +21,21 @@ if [[ -z "$comments" ]]; then
     exit 0
 fi
 
+# Fetch all issues
+issues=$(curl -sSL -H "Authorization: token $TOKEN" "$API/repos/$USERNAME/$REPO/issues?state=all&per_page=100")
+
+# Combine comments and issues
+all_data=$(echo "$comments" "$issues" | jq -s 'flatten')
+
 # Find the latest comment
-latest_comment=$(echo "$comments" | jq -r '[.[] | {created_at: .created_at, body: .body}] | sort_by(.created_at) | last | .body')
+latest_comment=$(echo "$all_data" | jq -r '[.[] | {created_at: .created_at, body: .body}] | sort_by(.created_at) | last | .body')
 
 # Check if latest_comment is empty
 if [[ -z "$latest_comment" ]]; then
-    echo "No latest comment found." > update_requirement.txt
+    echo "No latest comment found."
     exit 0
 fi
 
-# Update the comment in the text file
-echo "$latest_comment"
+# Output the comment to a text file
+echo "$latest_comment" > update_requirement.txt
+echo "Latest comment has been written to update_requirement.txt file."
