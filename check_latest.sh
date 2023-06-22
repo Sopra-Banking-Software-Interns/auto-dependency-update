87faf9486f8a5b51fd762923aaac2876cd07f706
Prakhar Mani Tripathi <91960314+CodePrakhar@users.noreply.github.com>
2023-06-22 19:57:24 +0530

    Update check_latest.sh

--- check_latest.sh
+++ check_latest.sh
@@ -6,7 +6,7 @@ echo "$json" | jq -c 'to_entries[]' | while IFS= read -r element; do
     # echo "Key: $key, Value: $value"
     cur=$(npm show $key version)
     # echo $cur
-    if [ "$cur" != "$value" ]; then
+    if [ "$cur" != "$value" ] && [ "^$cur" != "$value" ]; then
         echo "Update available for $key... Latest $cur available">>version_changes.txt
     else
         echo "$key is up to date">>version_changes.txt
