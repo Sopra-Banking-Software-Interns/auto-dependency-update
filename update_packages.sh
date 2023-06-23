8d235db3988c761b8406cd5b27644551c6116573
Prakhar Mani Tripathi <91960314+CodePrakhar@users.noreply.github.com>
2023-06-23 12:20:15 +0530

    Update update_packages.sh

--- update_packages.sh
+++ update_packages.sh
@@ -13,6 +13,9 @@ REPO_NAME=$(echo "$REPO_LINK" | awk -F'/' '{print $NF}' | sed 's/.git$//')
 echo "$REPO_OWNER"
 echo "$REPO_NAME"
 
+# Initialize an empty JSON object to store all the changes
+json_changes=$PACKAGE_JSON_FILE
+
 # Loop through each line in the text file
 while IFS= read -r line; do
   # Extract package name and latest version using string manipulation or other methods
@@ -21,25 +24,24 @@ while IFS= read -r line; do
   echo "$package_name"
   echo "$latest_version"
 
-  # Prepare the JSON payload for updating the package.json
-  json_payload=$(jq --arg package "$package_name" --arg version "$latest_version" '.dependencies[$package] = $version' "$PACKAGE_JSON_FILE")
-  echo "$json_payload"
+  # Update the JSON object with the package and version changes
+  json_changes=$(echo "$json_changes" | jq --arg package "$package_name" --arg version "$latest_version" '.dependencies[$package] = $version')
 
-  # Encode the JSON payload using base64
-  encoded_payload=$(echo "$json_payload" | base64 -w 0)
-
-  # Get the current SHA of the package.json file
-  current_sha=$(curl -s -u "$GITHUB_USERNAME:$GITHUB_TOKEN" "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/contents/$PACKAGE_JSON_FILE" | jq -r '.sha')
-
-  # Make a PUT request to the GitHub API to update the package.json file
-  curl -X PUT -u "$GITHUB_USERNAME:$GITHUB_TOKEN" \
-    -H "Accept: application/vnd.github+json" \
-    -d '{
-      "message": "Update package.json",
-      "content": "'"$encoded_payload"'",
-      "sha": "'"$current_sha"'"
-    }' "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/contents/$PACKAGE_JSON_FILE"
-
-  # Wait for a few seconds to avoid rate limiting (if necessary)
-  sleep 3
+# Wait for a few seconds to avoid rate limiting (if necessary)
+sleep 3
 done < "$TEXT_FILE"
+
+# Encode the JSON payload using base64
+encoded_payload=$(echo "$json_changes" | base64 -w 0)
+
+# Get the current SHA of the package.json file
+current_sha=$(curl -s -u "$GITHUB_USERNAME:$GITHUB_TOKEN" "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/contents/$PACKAGE_JSON_FILE" | jq -r '.sha')
+
+# Make a PUT request to the GitHub API to update the package.json file
+curl -X PUT -u "$GITHUB_USERNAME:$GITHUB_TOKEN" \
+  -H "Accept: application/vnd.github+json" \
+  -d '{
+    "message": "Update package.json",
+    "content": "'"$encoded_payload"'",
+    "sha": "'"$current_sha"'"
+  }' "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/contents/$PACKAGE_JSON_FILE"
